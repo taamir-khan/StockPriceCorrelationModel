06c3b15f18dcb10959d5849ac44e6c1f62335aa1
katepiotrowsk <81317656+katepiotrowsk@users.noreply.github.com>
2022-11-07 15:23:06 -0500

    Update main.r

--- main.r
+++ main.r
@@ -1,5 +1,22 @@
-# load dataset
-library(tidyverse)
-df <- read.csv("https://reneshbedre.github.io/assets/posts/reg/height.csv")
-# view first two rows
-head(df, 2)
+# reads csv file
+data <- read.csv("/Users/KatePiotrowski/Height_Model_R/JPM_Practice.csv")
+
+Name <- data$Name
+Date <- data$Date2
+Price <- data$Price
+y1 <- data$y1
+
+# set-up data frame with name, date, price, y1
+df <- data.frame(Name, Date, Price, y1)
+
+df$Month <- format(as.Date(Date, format="%Y-%m-%d"),"%m")
+df$Year <- format(as.Date(Date, format="%Y-%m-%d"),"%Y")
+
+df[order(as.Date(df$date, format="%Y-%m-%d"))]
+#df
+
+aggregate(y1~Month+Year,df,mean)
+
+cor(df$Price, df$y1,
+    method = "spearman"
+)
