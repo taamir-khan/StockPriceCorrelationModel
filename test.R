7e14be02f3eca6056c4007b8f9f0275d157e02b5
katepiotrowsk <81317656+katepiotrowsk@users.noreply.github.com>
2022-11-28 17:09:36 -0500

    Update test.R

--- test.R
+++ test.R
@@ -1,11 +1,15 @@
 library(data.table)
 library(dplyr)
-# sets path
-setwd("~/Desktop/senior design/r_model")
+library(corrr)
 
-# MSCI Data
+# sets path
+setwd("~/Correlation_Model")
+
+# gets the MSCI Data
+# MSCI data includes the ESG scores for each ticker
 msciData <- read.csv(file="MSCI_SUMMARY_SCORES3_Removed.csv")
 
+# assigning relevant variables from the csv file
 msciDate <- msciData$ASOF_DATE
 isinID <- msciData$ISSUER_ISIN
 industryAdjustedScore <- msciData$INDUSTRY_ADJUSTED_SCORE
@@ -15,27 +19,35 @@ environmentalPillarScore <- msciData$ENVIRONMENTAL_PILLAR_SCORE
 goverancePillarScore <- msciData$GOVERNANCE_PILLAR_SCORE
 socialPillarScore <- msciData$SOCIAL_PILLAR_SCORE
 
+# creates the data frame for the MSCI data
 df2 <- data.frame(msciDate, isinID, industryAdjustedScore, weightedAvgScore, environmentalPillarScore, goverancePillarScore, socialPillarScore)
 
+# retrieves the Month as a new variable from the given msciDate
 df2$Month <- format(as.Date(msciDate, format="%Y-%m-%d"),"%m")
+
+# retrieves the Year as a new variable from the given msciDate
 df2$Year <- format(as.Date(msciDate, format="%Y-%m-%d"),"%Y")
 
 df2[order(as.Date(df2$Date, format="%Y-%m-%d"))]
 group_mean2 <- aggregate(cbind(industryAdjustedScore, weightedAvgScore, environmentalPillarScore, goverancePillarScore, socialPillarScore) ~ isinID+Month+Year, data = df2, mean)
 #group_mean2
 
-# Price Data
+# now we are getting the Price data
 priceData <- read.csv(file="Price_Data3_ISIN.csv")
 
+# assigning relevant variables from the csv file
 ticker <- priceData$BB_TICKER_CD
 price <- priceData$PBD_PRICE_AMT
 priceDate <- priceData$DATE_DIM_ID
 isinID <- priceData$ISIN_CD
 
-# set-up data frame with name, date, price, y1
+# creates a new data frame for the Price Data
 df <- data.frame(priceDate, ticker, isinID, price)
 
+# retrieves the Month as a new variable from the given priceData
 df$Month <- format(as.Date(priceDate, format="%Y%m%d"),"%m")
+
+# retrieves the Year as a new variable from the given priceData
 df$Year <- format(as.Date(priceDate, format="%Y%m%d"),"%Y")
 
 df[order(as.Date(df$Date, format="%Y%m%d"))]
@@ -43,19 +55,24 @@ df[order(as.Date(df$Date, format="%Y%m%d"))]
 group_mean <- aggregate(price ~ isinID+ticker+Month+Year, data = df, mean)
 #group_mean
 
-# df3 <- merge(df,df2,by=c("Month","Year"))
-# df3
-# dplr <- left_join(df, df2, by=c("Month", "Year"))
-# dplr
-# setDT(group_mean2)[group_mean, c("industryAdjustedScore", "weightedAvgScore", "environmentalPillarScore", "goverancePillarScore", "socialPillarScore") := 
-#              .(industryAdjustedScore, weightedAvgScore, environmentalPillarScore, goverancePillarScore, socialPillarScore), on=c("Year", "Month", "price")]
-#df3 <- full_join(group_mean, group_mean2, by = c("isinID", "Month", "Year"), all = TRUE)
 df3 <- group_mean %>% inner_join( group_mean2, 
-                              by=c('isinID'='isinID', 'Year'='Year', 'Month'='Month'))
+                                  by=c('isinID'='isinID', 'Year'='Year', 'Month'='Month'))
 
-df3
-# x <- df3[5]
-# y <- df3[6:10]
-# correlation <- df3 %>%
-#   group_by(ticker) %>%
-#   summarize(cor=cor(x, y))
+
+# test <- cor(df3$price, df3$weightedAvgScore,
+#   method = "pearson")
+
+x <- df3[5]
+y <- df3[6:10]
+
+
+#single_corr <- cor(x, y)
+#single_corr
+
+#outputs duplicate correlation coefficents 
+correlate <- df3 %>%
+    group_by(ticker) %>%
+   summarize(r = cor(x, y))
+correlate
+#-0.05159548
+#-0.0542 
