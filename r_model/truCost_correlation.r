fc3d153a86870ebc50ab2ccb2a1e9753ca9e3a94
katepiotrowsk <81317656+katepiotrowsk@users.noreply.github.com>
2023-04-14 10:50:18 -0400

    Add files via upload

--- r_model/truCost_correlation.r
+++ r_model/truCost_correlation.r
@@ -5,12 +5,20 @@ library(tidyr)
 library(ggplot2)
 
 # Read stock price data
-stock_price_data <- read.csv("PriceData_2012_2022.csv")
+stock_price_data <- read.csv(file="/Users/KatePiotrowski/Correlation_Model/Data_Input/PriceData_2012_2022.csv")
 
 # Convert DATE_DIM_ID to a date format and extract month and year
 stock_price_data$DATE_DIM_ID <- as.Date(as.character(stock_price_data$DATE_DIM_ID), format = "%Y%m%d")
 stock_price_data$year_month <- format(stock_price_data$DATE_DIM_ID, "%Y-%m")
 stock_price_data$PBD_PRICE_AMT <- as.numeric(stock_price_data$PBD_PRICE_AMT)
+ticker <- stock_price_data$BB_TICKER_CD
+
+# data frame of ticker and isin_ID
+Ticker_and_isinID <- stock_price_data[, c(2, 3)]
+
+# get distinct values of ticker and isin_ID
+#distinct_ticker_isinID <- Ticker_and_isinID %>% distinct()
+distinct_ticker_isinID <- unique(Ticker_and_isinID)
 
 # Calculate the monthly average price
 monthly_avg_price <- stock_price_data %>%
@@ -26,7 +34,7 @@ monthly_avg_price <- monthly_avg_price %>%
   filter(ISIN_CD != "")
 
 # Read TruCost data
-trucost_data <- read.csv("truCost.csv")
+trucost_data <- read.csv(file="/Users/KatePiotrowski/Correlation_Model/Data_Input/truCost.csv")
 
 # Convert ASOF_DATE to a date format and extract month and year
 trucost_data$ASOF_DATE <- as.POSIXct(trucost_data$ASOF_DATE, format = "%Y-%m-%d %H:%M:%S")
@@ -65,15 +73,33 @@ correlations <- combined_data %>%
     carbon_emissions_scope_2_correlation = cor(avg_price, avg_scope2, use = "complete.obs")
   )
 
-# Save the final dataframe to a CSV file
-write.csv(correlations, "correlations.csv", row.names = FALSE)
+# Add the ticker name into the data frame
+correlations_with_ticker <- merge(distinct_ticker_isinID, correlations, by="ISIN_CD")
+
+#correlations_t <- distinct(correlations_with_ticker)
+#unique_tickers <- unique(correlations_with_ticker$BB_TICKER_CD)
+unique_tickers <- correlations_with_ticker %>% distinct(BB_TICKER_CD)
+
+# this outputs the correlations with Tickers
+my_data_unique <- correlations_with_ticker[correlations_with_ticker$BB_TICKER_CD %in% unique_tickers$BB_TICKER_CD, ]
+
+# Use complete.cases() to identify rows with no missing values
+valid_rows <- complete.cases(my_data_unique)
+
+# Subset the original data frame to keep only valid rows
+df_clean <- my_data_unique[valid_rows, ]
+
+#df_clean_tickers <- subset(df_clean, complete.cases(df_clean[, "BB_TICKER_CD"]))
+
+# Save the final data frame to a CSV file
+#write.csv(df_clean, "TruCost_to_Price_Correlation.csv", row.names = FALSE)
 
 # Plot graphs
 for (isin in unique(combined_data$ISIN_CD)) {
   # Filter data for the specific ISIN
   isin_data <- combined_data %>%
     filter(ISIN_CD == isin)
-
+  
   # Price and Carbon Score Plot
   price_carbon_plot <- ggplot(isin_data, aes(x = as.Date(year_month, format = "%Y-%m"))) +
     geom_line(aes(y = avg_price, color = "Price")) +
@@ -87,9 +113,8 @@ for (isin in unique(combined_data$ISIN_CD)) {
       y = "Price",
       color = "Legend"
     )
-
+  
   # Save plot as a PNG file
-  ggsave(paste0("price_carbon_scores_", isin, ".png"), plot = price_carbon_plot, width = 10, height = 6, units = "in")
+  #ggsave(paste0("price_carbon_scores_", isin, ".png"), plot = price_carbon_plot, width = 10, height = 6, units = "in")
 }
 
-                  
